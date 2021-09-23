96962a5d7686c39f68d22a39e001bef2ef86af04
AbdullahPrasetio <adeprastio1996@gmail.com>
2021-09-23 15:54:53 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ number }}
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const number = ref(0);
+
+    setInterval(() => {
+      number.value++;
+    }, 1000);
+    return { number };
+  },
+};
+</script>
